0f3e87fd1fbde1429e59a81db71efd2e26db87d5
cvs2svn conversion <cvs2svn-artefacts-noreply@gphoto.org>
2001-12-02 02:27:01 +0000

    This commit was manufactured by cvs2svn to create branch 'hun'.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/branches/hun/libgphoto2@3329 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/sierra/sierra-usbwrap.h
+++
@@ -1,9 +0,0 @@
-/*
- * sierra_usbwrap.h
- */
-
-#include "gphoto2.h"
-
-int usb_wrap_write_packet(gp_port* dev, char* sierra_msg, int sierra_len);
-int usb_wrap_read_packet(gp_port* dev, char* sierra_response, int sierra_len);
-
