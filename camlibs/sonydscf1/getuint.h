63e1b14d34d349e9e890e0079e8d87ba376530ac
cvs2svn conversion <cvs2svn-artefacts-noreply@gphoto.org>
2002-01-11 14:50:07 +0000

    This commit was manufactured by cvs2svn to create tag 'Before_GPFileSystem'.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/tags/Before_GPFileSystem/libgphoto2@3816 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/sonydscf1/getuint.h
+++
@@ -1,2 +0,0 @@
-u_short get_u_short P__((u_char *));
-u_int get_u_int P__((u_char *));
--- frontends/command-line/options.h
+++
@@ -1,5 +0,0 @@
-int option_is_present (char *op, int argc, char **argv);
-int verify_options (int argc, char **argv);
-int execute_options (int argc, char **argv);
-void usage (void);
-void print_version (void);
