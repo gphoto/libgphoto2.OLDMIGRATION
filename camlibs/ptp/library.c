2e09ca2ad6c2730556e33715dffda340a4de1204
Hans Ulrich Niedermann <hun@n-dimensional.de>
2002-11-22 15:12:23 +0000

    2002-11-22 Mariusz Woloszyn
    	* library.c: added several Kodak cameras.
    	  Changed naming convention of Kodak devices. Dash removed (less
      	  readable, but conforms to original Kodak naming scheme).
    
    
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/branches/libgphoto2-2_1_1/libgphoto2@5746 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/ptp/library.c
+++ camlibs/ptp/library.c
@@ -181,21 +181,31 @@ static struct {
 	 * The very first PTP camera (with special firmware only), also
 	 * called "PTP Prototype", may report non PTP interface class
 	 */
-	{"Kodak DC-240 (PTP)",  0x040a, 0x0121},
+	{"Kodak DC240 (PTP)",  0x040a, 0x0121},
 	/*
-	 * Old DC-4800 firmware reported cutom interface class, so we have
+	 * Old DC4800 firmware reported custom interface class, so we have
 	 * to detect it by product/vendor IDs
 	 */
-	{"Kodak DC-4800", 0x040a, 0x0160},
+	{"Kodak DC4800", 0x040a, 0x0160},
 	/* Below other camers known to be detected by interface class */
 
-	{"Kodak DX-3215", 0x040a, 0x0525},
-	{"Kodak DX-3500", 0x040a, 0x0500},
-	{"Kodak DX-3600", 0x040a, 0x0510},
-	{"Kodak DX-3700", 0x040a, 0x0530},
-	{"Kodak DX-3900", 0x040a, 0x0170},
-	{"Kodak DX-4900", 0x040a, 0x0550},
+ 	{"Kodak DC3900", 0x040a, 0x0170},
+	{"Kodak DX3215", 0x040a, 0x0525},
+	{"Kodak DX3500", 0x040a, 0x0500},
+	{"Kodak DX3600", 0x040a, 0x0510},
+	{"Kodak DX3700", 0x040a, 0x0530},
+	{"Kodak DX3900", 0x040a, 0x0170},
+	{"Kodak DX4230", 0x040a, 0x0535},
+	{"Kodak DX4300", 0x040a, 0x0566},
+	{"Kodak DX4330", 0x040a, 0x0555},
+	{"Kodak DX4900", 0x040a, 0x0550},
 	{"Kodak MC3", 0x040a, 0x0400},
+ 	{"Kodak LS420", 0x040a, 0x0540},
+ 	{"Kodak CX4210", 0x040a, 0x0560},
+ 	{"Kodak CX4230", 0x040a, 0x0535},
+
+
+	/* HP PTP cameras */
 	{"HP PhotoSmart 318", 0x03f0, 0x6302},
 	{"HP PhotoSmart 612", 0x03f0, 0x6302},
 	{"HP PhotoSmart 715",  0x03f0, 0x6402},
