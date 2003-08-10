b8482ce1a17bdc5fa8141ef596a16b08e6fd63e3
cvs2svn conversion <cvs2svn-artefacts-noreply@gphoto.org>
2003-08-10 18:46:01 +0000

    This commit was manufactured by cvs2svn to create tag 'libgphoto2-2_1_2-rc6'.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/tags/libgphoto2-2_1_2-rc6/libgphoto2@6634 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/canon/crc.c
+++ camlibs/canon/crc.c
@@ -4,8 +4,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "config.h"
 #include "crc.h"
 
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#  undef _
+#  define _(String) dgettext (GETTEXT_PACKAGE, String)
+#  ifdef gettext_noop
+#    define N_(String) gettext_noop (String)
+#  else
+#    define N_(String) (String)
+#  endif
+#else
+#  define _(String) (String)
+#  define N_(String) (String)
+#endif
 
 /*
  * CRC 0102010
