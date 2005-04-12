83e29d4b0d1d8f24e520e2a29c1b76ac1cccbcb7
cvs2svn conversion <cvs2svn-artefacts-noreply@gphoto.org>
2005-04-12 23:57:17 +0000

    This commit was manufactured by cvs2svn to create tag 'dynlibtest-0_4_3-release'.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/tags/dynlibtest-0_4_3-release/libgphoto2@7810 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/stv0680/sharpen.h
+++
@@ -1,7 +0,0 @@
-#ifndef __STV680_SHARPEN_H
-#define __STV680_SHARPEN_H
-void  sharpen(int width, int height,
-	unsigned char *src_region, unsigned char *dest_region, 
-	int sharpen_percent
-);
-#endif
