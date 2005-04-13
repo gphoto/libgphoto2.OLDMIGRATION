7a2f3580717d7373f671b5b1cc7f79adf707d1cf
cvs2svn conversion <cvs2svn-artefacts-noreply@gphoto.org>
2005-04-13 04:08:58 +0000

    This commit was manufactured by cvs2svn to create tag 'dynlibtest-0_4_7-release'.
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/tags/dynlibtest-0_4_7-release/libgphoto2@7818 67ed7778-7388-44ab-90cf-0a291f65f57c

--- camlibs/sonydscf1/chotplay.h
+++
@@ -1,9 +0,0 @@
-int write_file(u_char *buf, int len, FILE *outfp);
-int make_jpeg_comment(u_char *buf, u_char *jpeg_comment);
-int get_picture_information (int *, int);
-long get_file(char *name, char **data, int format, int verbose);
-long get_thumbnail(char *name, char **data, int format, int verbose, int n);
-void get_date_info (char *, char * ,char *);
-long get_picture (int, char **, int, int, int);
-void get_all_pictures (int, int, char *, int, int);
-void delete_picture (int, int);
